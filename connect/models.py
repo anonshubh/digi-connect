399a4e1ac854b425f3544aece290ce13bb21e0ac
Shubh Pathak <shubhpathak07@gmail.com>
2021-04-06 01:37:54 +0530

    Added Request Models

--- connect/models.py
+++ connect/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class GenreField(models.Model):
+    """
+    Different Genre For Each Sectors
+    """
+    genre_type = models.CharField(max_length=56)
+
+
+    def __str__(self):
+        return (f'{self.genre_type}')[:10]
+
+
+class SectorField(models.Model):
+    """
+    Relation For Each Sector, Example: Travel, Sports"
+    """
+    name = models.CharField(max_length=56)
+    genre = models.ManyToManyField(GenreField,related_name='sec')
+
+
+    def __str__(self):
+        return (f'{self.name}')[:10]
+
+
+
+class Request(models.Model):
+    """
+    Relation For Request Posted By User
+    """
+    requester = models.ForeignKey(User,on_delete=models.CASCADE,blank=True,related_name='req')
+    deadline = models.DateTimeField()
+    subject = models.CharField(max_length=128)
+    content = models.TextField()
+    sector = models.ForeignKey(SectorField,on_delete=models.CASCADE,related_name='reqsec')
+
+
+    def __str__(self):
+        subject = (self.subject)[:10]
+        return f'By {self.requester}: {subject}'
+
--- profiles/models.py
+++ profiles/models.py
@@ -23,36 +23,7 @@ class UserInfo(models.Model):
     year = models.IntegerField()
     created_at = models.DateTimeField(auto_now=True)
     modified_at = models.DateTimeField(auto_now_add=True)
+    is_restricted = models.BooleanField(default=False) 
 
     def __str__(self):
-        return f"Info Of {self.user}"
-
-
-
-# @receiver(email_confirmed)
-# def user_info_creation(request,email_address,**kwargs):
-#     user = email_address.user
-#     print(kwargs)
-#     # email = user.email
-#     # index = email.find('@')
-#     # dot = email.find('.')
-#     # domain = email[index+1:dot]
-#     # if(domain == 'iiitdm'):
-#     #     inst_obj = Institute.objects.get(domain=domain)
-#     #     department = email[:3]
-#     #     year = email[3:5]
-#     #     year_ = "20"+year
-#     #     if(year=="19"):
-#     #         if(department=='mfd' or department=='mpd' or department=='mdm'):
-#     #             department="mdm/mpd/mfd"
-#     #         elif(department=='coe' or department=='ced'):
-#     #             department="coe/ced"
-#     #         elif(department=='edm' or department=='evd' or department=='esd'):
-#     #             department="edm/evd/esd"
-#     #Addition or selection of another Institute will be in 'else' clause 
-
-#     # user_obj = UserInfo.objects.create(
-#     #     user=user,
-#     # )
-
-
+        return f"Info OF {self.user}"
